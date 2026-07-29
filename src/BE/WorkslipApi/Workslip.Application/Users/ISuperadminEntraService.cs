67cf435cf065651a943899517402a4bcc2318e47
rasm105k <31623093+rasm105k@users.noreply.github.com>
2026-07-29 15:56:37 +0200

    WOR-107 add Superadmin Entra provisioning contract

---
+++ src/BE/WorkslipApi/Workslip.Application/Users/ISuperadminEntraService.cs
@@ -0,0 +1,9 @@
+namespace Workslip.Application.Users;
+
+public interface ISuperadminEntraService : IUserEntraService
+{
+    Task<CreateEntraUserResult> EnsureSuperadminAsync(
+        string email,
+        string displayName,
+        CancellationToken cancellationToken);
+}
