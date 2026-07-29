b7aa8a26b613c26794f50ee59e506019be032952
rasm105k <31623093+rasm105k@users.noreply.github.com>
2026-07-29 12:49:17 +0200

    WOR-198: defer non-critical startup integrations

--- src/FE/src/main.tsx
+++ src/FE/src/main.tsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './registerSW'
-import './fonts.css'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './fonts.css';
+import './index.css';
+import App from './App.tsx';
 import { initializeApplicationInsights, installGlobalApplicationInsightsHandlers } from './applicationInsights';
-
-initializeApplicationInsights();
-installGlobalApplicationInsightsHandlers();
+import { scheduleAfterInitialLoad } from './lib/scheduleAfterInitialLoad';
 
 if (typeof window !== 'undefined') {
   // Vite emits this event when an already-open client references a hashed lazy
@@ -43,4 +40,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
+
+scheduleAfterInitialLoad(() => {
+  void import('./registerSW');
+  void initializeApplicationInsights().then(installGlobalApplicationInsightsHandlers);
+});
