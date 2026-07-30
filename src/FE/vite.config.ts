5936090eaff4bcb7542f855b955a9f48fd64bc74
rasm105k <31623093+rasm105k@users.noreply.github.com>
2026-07-30 13:00:50 +0200

    WOR-213: use prompt lifecycle with automatic fallback

--- src/FE/vite.config.ts
+++ src/FE/vite.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: 'prompt',
       includeAssets: ['favicon.svg'],
       strategies: 'injectManifest',
       srcDir: 'src',
