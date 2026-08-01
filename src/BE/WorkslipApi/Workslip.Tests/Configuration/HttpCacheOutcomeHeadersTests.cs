4aa6085ad40b4070919de367dd4e32b8e4812dfb
rasm105k <31623093+rasm105k@users.noreply.github.com>
2026-08-01 22:34:34 +0200

    test(cache): cover HTTP cache outcome headers

---
+++ src/BE/WorkslipApi/Workslip.Tests/Configuration/HttpCacheOutcomeHeadersTests.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Http;
+using Workslip.Api.Endpoints;
+
+namespace Workslip.Tests.Configuration;
+
+public sealed class HttpCacheOutcomeHeadersTests
+{
+    [Fact]
+    public void SetNoStore_marks_response_as_cache_bypass()
+    {
+        var context = new DefaultHttpContext();
+
+        HttpCacheHeaders.SetNoStore(context);
+
+        Assert.Equal("bypass", context.Response.Headers["X-Workslip-Cache"].ToString());
+    }
+
+    [Fact]
+    public void Missing_validator_marks_response_as_cache_miss()
+    {
+        var context = new DefaultHttpContext();
+
+        var matches = HttpCacheHeaders.MatchesIfNoneMatch(context, "W/\"abc\"");
+
+        Assert.False(matches);
+        Assert.Equal("miss", context.Response.Headers["X-Workslip-Cache"].ToString());
+    }
+
+    [Fact]
+    public void Matching_validator_marks_response_as_revalidated()
+    {
+        var context = new DefaultHttpContext();
+        context.Request.Headers.IfNoneMatch = "W/\"abc\"";
+
+        var matches = HttpCacheHeaders.MatchesIfNoneMatch(context, "W/\"abc\"");
+
+        Assert.True(matches);
+        Assert.Equal("revalidated", context.Response.Headers["X-Workslip-Cache"].ToString());
+    }
+}
