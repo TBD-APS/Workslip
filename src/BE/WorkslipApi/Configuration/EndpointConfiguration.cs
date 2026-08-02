5c70e14a208495a9059a70f4db6eb1f3b1cc045a
rasm105k <31623093+rasm105k@users.noreply.github.com>
2026-08-02 02:00:45 +0200

    feat(diagnostics): map Superadmin error endpoint

--- src/BE/WorkslipApi/Configuration/EndpointConfiguration.cs
+++ src/BE/WorkslipApi/Configuration/EndpointConfiguration.cs
@@ -25,6 +25,7 @@ public static class EndpointConfiguration
         app.MapReferenceDataEndpoints();
         app.MapPushNotificationEndpoints();
         app.MapCacheEndpoints();
+        app.MapDiagnosticsEndpoints();
 
         return app;
     }
