f26c02cbb7a0d7172b0ad56f6222e1ad1703da36
rasm105k <31623093+rasm105k@users.noreply.github.com>
2026-08-08 14:55:57 +0200

    RBJ-321: restore seeder compile namespaces

---
+++ src/BE/WorkslipApi/Workslip.Infrastructure/Seeding/DevelopmentDatabaseSeeder.GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using System.Data;
+global using System.Runtime.ExceptionServices;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Storage;
+global using Workslip.Domain;
+global using Workslip.Domain.Models;
