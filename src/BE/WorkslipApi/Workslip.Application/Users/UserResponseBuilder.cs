1a0821fa5c611c084ca305d908448c86ac09a0c5
rasm105k <31623093+rasm105k@users.noreply.github.com>
2026-08-10 18:23:36 +0200

    RBJ-357 map user audience kind

--- src/BE/WorkslipApi/Workslip.Application/Users/UserResponseBuilder.cs
+++ src/BE/WorkslipApi/Workslip.Application/Users/UserResponseBuilder.cs
@@ -1,19 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Workslip.Domain.Models;
 
-namespace Workslip.Application.Users
+namespace Workslip.Application.Users;
+
+public static class UserResponseBuilder
 {
-    public static class UserResponseBuilder
-    {
-        public static UserResponse MapToResponse(UserDataRow user) =>
-    new(
-        user.Id,
-        user.OrganizationId,
-        user.Email,
-        user.DisplayName,
-        user.Phone,
-        user.Role);
-    }
+    public static UserResponse MapToResponse(UserDataRow user) =>
+        new(
+            user.Id,
+            user.OrganizationId,
+            user.Email,
+            user.DisplayName,
+            user.Phone,
+            user.Role,
+            user.UserKind);
 }
