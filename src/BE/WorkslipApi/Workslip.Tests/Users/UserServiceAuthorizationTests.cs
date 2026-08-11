b157cb3da74a9c1be746614a78ce9ddd7e87d48d
rasm105k <31623093+rasm105k@users.noreply.github.com>
2026-08-11 18:45:34 +0200

    RBJ-335 provide image storage in user authorization tests

--- src/BE/WorkslipApi/Workslip.Tests/Users/UserServiceAuthorizationTests.cs
+++ src/BE/WorkslipApi/Workslip.Tests/Users/UserServiceAuthorizationTests.cs
@@ -5,6 +5,7 @@ using Workslip.Application.Users;
 using Workslip.Application.Users.Validators;
 using Workslip.Domain;
 using Workslip.Domain.Models;
+using Workslip.Tests.TestDoubles;
 using Xunit;
 
 namespace Workslip.Tests.Users;
@@ -118,6 +119,7 @@ public sealed class UserServiceAuthorizationTests
             new UpdateUserRequestValidator(),
             entra,
             new FakeCurrentUserContext(actorRole),
+            new NoOpImageStorage(),
             NullLogger<UserService>.Instance);
 
     private static UserDataRow CreateUser(string role) =>
