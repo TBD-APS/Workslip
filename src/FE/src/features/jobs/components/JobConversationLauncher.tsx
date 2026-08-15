fe3dd8f49a582b58fec2057c15171decaca93739
rasm105k <31623093+rasm105k@users.noreply.github.com>
2026-08-15 21:49:58 +0200

    RBJ-551: Load conversation launcher layout styles

--- src/FE/src/features/jobs/components/JobConversationLauncher.tsx
+++ src/FE/src/features/jobs/components/JobConversationLauncher.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { MessageCircle } from 'lucide-react';
 import { useSearchParams } from 'react-router-dom';
 import { JobConversationDrawer } from './JobConversationDrawer';
+import './JobConversationLauncher.css';
 
 type JobConversationLauncherProps = {
   jobId: string;
