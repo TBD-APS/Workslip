84cfdb397ef50823d6b32a2ff5b11cde617d6782
rasm105k <31623093+rasm105k@users.noreply.github.com>
2026-08-16 07:57:11 +0200

    RBJ-588 add executive leadership roles

--- platform/mr-saasy-control-plane/app/AI/Application/Routing/AgentRole.php
+++ platform/mr-saasy-control-plane/app/AI/Application/Routing/AgentRole.php
@@ -4,6 +4,13 @@ namespace App\AI\Application\Routing;
 
 enum AgentRole: string
 {
+    case ChiefExecutive = 'chief_executive';
+    case ChiefOperatingOfficer = 'chief_operating_officer';
+    case ChiefTechnologyOfficer = 'chief_technology_officer';
+    case ChiefProductOfficer = 'chief_product_officer';
+    case ChiefMarketingGrowth = 'chief_marketing_growth';
+    case ChiefFinanceCommercial = 'chief_finance_commercial';
+
     case EngineeringOrchestrator = 'engineering_orchestrator';
     case SoftwareArchitect = 'software_architect';
     case ImplementationComplex = 'implementation_complex';
