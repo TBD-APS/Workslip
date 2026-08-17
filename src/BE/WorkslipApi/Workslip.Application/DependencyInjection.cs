2060757e148ecdd1d5fa2944f8c0cdfcb3da7fc4
rasm105k <31623093+rasm105k@users.noreply.github.com>
2026-08-17 21:30:04 +0200

    WOR-550 register job lifecycle service

--- src/BE/WorkslipApi/Workslip.Application/DependencyInjection.cs
+++ src/BE/WorkslipApi/Workslip.Application/DependencyInjection.cs
@@ -20,6 +20,7 @@ public static class DependencyInjection
 public static IServiceCollection AddWorkslipApplication(this IServiceCollection services)
 {
     services.AddScoped<JobService>();
+    services.AddScoped<JobLifecycleService>();
     services.AddScoped<AuthorizedJobService>();
     services.AddScoped<IJobService, QualityIntegrityJobService>();
     services.AddScoped<IJobOverviewService, JobOverviewService>();
