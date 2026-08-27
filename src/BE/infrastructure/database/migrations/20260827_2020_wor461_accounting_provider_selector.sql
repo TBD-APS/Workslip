739e2ee86e53a7e4bbf6d1b5fecd9f8ac9c3284d
rasm105k <31623093+rasm105k@users.noreply.github.com>
2026-08-27 22:25:35 +0200

    RBJ-461 create dedicated organization accounting settings table

--- src/BE/infrastructure/database/migrations/20260827_2020_wor461_accounting_provider_selector.sql
+++ src/BE/infrastructure/database/migrations/20260827_2020_wor461_accounting_provider_selector.sql
@@ -3,10 +3,17 @@ SET NOCOUNT ON;
 
 BEGIN TRANSACTION;
 
-IF COL_LENGTH('dbo.Organizations', 'AccountingProviderId') IS NULL
+IF OBJECT_ID('dbo.OrganizationAccountingSettings', 'U') IS NULL
 BEGIN
-    ALTER TABLE dbo.Organizations
-        ADD AccountingProviderId nvarchar(80) NULL;
+    CREATE TABLE dbo.OrganizationAccountingSettings
+    (
+        OrganizationId uniqueidentifier NOT NULL,
+        ProviderId nvarchar(80) NOT NULL,
+        UpdatedAt datetimeoffset NOT NULL,
+        CONSTRAINT PK_OrganizationAccountingSettings PRIMARY KEY (OrganizationId),
+        CONSTRAINT FK_OrganizationAccountingSettings_Organizations
+            FOREIGN KEY (OrganizationId) REFERENCES dbo.Organizations(Id) ON DELETE CASCADE
+    );
 END;
 
 COMMIT TRANSACTION;
