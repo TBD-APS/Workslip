4367ac72eb95437c32fe9838c5d65a238cc5fcdc
rasm105k <31623093+rasm105k@users.noreply.github.com>
2026-08-27 22:24:56 +0200

    RBJ-461 keep accounting settings outside core organization EF model

--- src/BE/WorkslipApi/Workslip.Domain/Models/OrganizationRow.cs
+++ src/BE/WorkslipApi/Workslip.Domain/Models/OrganizationRow.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace Workslip.Domain.Models;
 
 public sealed class OrganizationRow
@@ -7,10 +5,6 @@ public sealed class OrganizationRow
     public Guid Id { get; init; }
     public string Name { get; init; } = "";
     public string Cvr { get; init; } = "";
-
-    [MaxLength(80)]
-    public string? AccountingProviderId { get; set; }
-
     public DateTimeOffset CreatedAt { get; init; }
     public DateTimeOffset UpdatedAt { get; init; }
     public ICollection<OrganizationFilialRow> Filials { get; set; } = [];
