2bf28bb85de7ca768c8a89230d0075d0bde0e8d6
rasm105k <31623093+rasm105k@users.noreply.github.com>
2026-08-29 00:42:40 +0200

    fix(release-6.2): regenerate frontend API client (#966)

--- src/FE/src/api/generated/models/index.ts
+++ src/FE/src/api/generated/models/index.ts
@@ -85,7 +85,6 @@ export * from './getApiLeaderAnalysisEconomicsSummaryParams';
 export * from './getApiNotificationsParams';
 export * from './getApiSuperadminUsersParams';
 export * from './getApiUsersParams';
-export * from './getApiWorksheetsAllDocumentsUserIdParams';
 export * from './getApiWorksheetsAllParams';
 export * from './getApiWorksheetsAllReportPdfParams';
 export * from './getApiWorksheetsAllReportPdfPreviewParams';
