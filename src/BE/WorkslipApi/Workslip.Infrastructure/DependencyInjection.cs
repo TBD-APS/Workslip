a82bea641c150e8f81a61ef9c9e6555e500bbc3b
rasm105k <31623093+rasm105k@users.noreply.github.com>
2026-08-29 15:39:46 +0200

    feat(control-center): publish sanitized Bug Radar checkpoints

--- src/BE/WorkslipApi/Workslip.Infrastructure/DependencyInjection.cs
+++ src/BE/WorkslipApi/Workslip.Infrastructure/DependencyInjection.cs
@@ -147,6 +147,14 @@ public static class DependencyInjection
             client.DefaultRequestHeaders.UserAgent.ParseAdd("Workslip-ControlCenter/1.0");
         });
 
+        services.AddOptions<MrSaasyBugRadarOptions>()
+            .Configure<IConfiguration>((options, config) =>
+                config.GetSection(MrSaasyBugRadarOptions.SectionName).Bind(options));
+        services.AddHttpClient<MrSaasyBugRadarCheckpointPublisher>(client =>
+        {
+            client.Timeout = TimeSpan.FromSeconds(10);
+        });
+
         services.AddScoped<IEmailService, AcsEmailService>();
         services.AddSingleton<VapidKeyMaterial>();
         services.AddSingleton<IVapidPublicKeyProvider>(serviceProvider =>
@@ -168,6 +176,7 @@ public static class DependencyInjection
             services.AddHostedService<InviteEntraCleanupService>();
             services.AddHostedService<PushNotificationWorker>();
             services.AddHostedService<PowerBiWorksheetExportWorker>();
+            services.AddHostedService<MrSaasyBugRadarPublisherWorker>();
         }
 
         services.AddOptions<VapidOptions>()
